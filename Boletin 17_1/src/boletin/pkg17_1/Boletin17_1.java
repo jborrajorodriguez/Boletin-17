0d468ce3560d2304d0e48834fb94da591fd86f53
jborrajorodriguez <jborrajorodriguez@victoria22.danielcastelao.org>
2018-01-22 08:40:40 +0100

    Clase main

---
+++ Boletin 17_1/src/boletin/pkg17_1/Boletin17_1.java
@@ -0,0 +1,13 @@
+package boletin.pkg17_1;
+/**
+ * @author Juan Borrajo Rodriguez Nº5937
+ */
+public class Boletin17_1 {
+
+    public static void main(String[] args) {
+     Numeros eje1=new Numeros();
+     eje1.arrayNum();
+
+    }
+
+}
