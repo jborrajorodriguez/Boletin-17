7ea1e7b3aebad12d9629fd5d64adde68036c9c81
jborrajorodriguez <jborrajorodriguez@victoria22.danielcastelao.org>
2018-01-25 13:10:05 +0100

    Clase main

---
+++ Boletin 17_4/src/boletin/pkg17_4/Boletin17_4.java
@@ -0,0 +1,15 @@
+package boletin.pkg17_4;
+/**
+ * @author Juan Borrajo Rodriguez Nº5937
+ */
+public class Boletin17_4 {
+
+    public static void main(String[] args) {
+        Dni a=new Dni();
+        a.calcularLetra(2135489);
+        
+        
+
+    }
+
+}
